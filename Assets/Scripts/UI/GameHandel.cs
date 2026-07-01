9769af221243823000690184ac74021afa2181ea
LAPTOP-OUBIKT97\Manmoy Dasgupta <manmoydasgupta12@gmail.com>
2026-07-01 21:45:45 +0530

    removed every files and scripts

--- Assets/Scripts/UI/GameHandel.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GameHandel : MonoBehaviour
-{
-    private void Start()
-    {
-        Debug.Log("[GameHandel] Placeholder component active. No health system is configured on this project yet.", this);
-    }
-}
